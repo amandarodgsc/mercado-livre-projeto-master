e3cd68b431a18dd1a23ef5f17d4f6e9f5f847937
amandarodgsc <amandarodgsc30@gmail.com>
2024-08-20 13:40:10 -0300

    banner

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useContext } from 'react';
-
 import './Products.css';
 import fetchProducts from '../../api/fetchProducts';
 import ProductCard from '../ProductCard/ProductCard';
@@ -7,24 +6,33 @@ import Loading from '../Loading/Loading';
 import AppContext from '../../context/AppContext';
 
 function Products() {
-
   const { products, setProducts, loading, setLoading } = useContext(AppContext);
-  
 
   useEffect(() => {
+    setLoading(true); // Ensure loading is set to true before fetching data
     fetchProducts('iphone').then((response) => {
       setProducts(response);
       setLoading(false);
     });
-  }, []);
+  }, [setProducts, setLoading]);
 
   return (
-    (loading && <Loading /> ) || (
-      <section className="products container">
-        {products.map((product) => <ProductCard key={product.id} data={product} />)}
-      </section>
-    )
-    
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <div className="banner">
+            <img src="https://http2.mlstatic.com/D_NQ_938152-MLA78305091572_082024-OO.webp" alt="Product Banner" />
+          </div>
+          <section className="products container">
+            {products.map((product) => (
+              <ProductCard key={product.id} data={product} />
+            ))}
+          </section>
+        </div>
+      )}
+    </>
   );
 }
 
