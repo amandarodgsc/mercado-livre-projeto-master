425ba960e8ae4bf00aefc465bdb25b95378e3208
amandarodgsc <amandarodgsc30@gmail.com>
2024-10-02 12:06:26 -0300

    att para mercado livre vendedor

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,22 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
 import Cart from './components/Cart/Cart';
-import Pagamento from './components/Pagamento/pagamento';
 import SiteShoppingInfo from './components/SiteShoppingInfo/SiteShoppingInfo';
-import Banner from './components/Banner/Banner';
-import VideoBanner from './components/Banner/VideoBanner';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Pagamento from './components/Pagamento/pagamento'
 
 function App() {
   return (
-    <Provider>
     <Router>
          <Header />
-         <Banner />
+        
    <Routes>
              <Route path="/pagamento" element={<Pagamento />} />
            </Routes>
-      <Products />
-         <Cart />
-         <VideoBanner/>
+          
          <SiteShoppingInfo/>
       </Router>
-       </Provider>
+      
   )
 }
 
