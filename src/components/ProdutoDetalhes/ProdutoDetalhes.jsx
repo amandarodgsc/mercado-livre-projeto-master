fed0e67186e16a5e19f9831e2e99134733c2539c
amandarodgsc <amandarodgsc30@gmail.com>
2024-10-29 11:29:53 -0300

    pagina detalhes atualizada

--- src/components/ProdutoDetalhes/ProdutoDetalhes.jsx
+++ src/components/ProdutoDetalhes/ProdutoDetalhes.jsx
@@ -1,6 +1,7 @@
 // src/components/ProdutoDetalhes/ProdutoDetalhes.js
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import './ProdutosDetalhes.css';
 
 function ProdutoDetalhes() {
   const { id } = useParams(); // Acessa o ID do produto da URL
@@ -18,12 +19,18 @@ function ProdutoDetalhes() {
 
   return (
     <div className="produto-detalhes">
-      <img src={product.image} alt={product.name} />
-      <h1>{product.name}</h1>
-      <p>Preço: R$ {product.price.toFixed(2)}</p>
-      <p>Quantidade: {product.quantity}</p>
-      <p>Categoria: {product.category}</p>
-      <p>Descrição: {product.description}</p>
+      <div className="produto-imagem">
+        <img src={product.image} alt={product.name} />
+      </div>
+      <div className="produto-info">
+        <h1>{product.name}</h1>
+        <p className="produto-preco">R$ {product.price.toFixed(2)}</p>
+        <p className="produto-desconto">36% OFF</p>
+        <p className="produto-categoria">Categoria: {product.category}</p>
+        <p className="produto-descricao">{product.description}</p>
+        <button className="comprar-agora">Comprar agora</button>
+        <button className="adicionar-carrinho">Adicionar ao carrinho</button>
+      </div>
     </div>
   );
 }
